6e23037e1f3a822aef4f866b2fdc19767daa2525
unknown <rosit.lawrence@gmail.com>
2025-11-18 17:35:45 +0800

    E-commerce Chapter 2

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OrderItem extends Model
 {
-    //
+    protected $fillable = ['product_id', 'order_id', 'quantity', 'unit_amount', 'total_amount'];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- database/migrations/2025_11_18_073203_create_products_table.php
+++ database/migrations/2025_11_18_073203_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,17 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->foreignId('brand_id')->constrained('brands')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->json('image')->nullable();
+            $table->longText('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_feature')->default(false);
+            $table->boolean('in_stock')->default(true);
+            $table->boolean('on_sale')->default(false);
             $table->timestamps();
         });
     }
