ee699ccbec46432a84770c33314026dd436f5cef
unknown <rosit.lawrence@gmail.com>
2025-11-20 14:39:17 +0800

    E-commerce 6

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -2,28 +2,177 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\OrderResource\Pages;
-use App\Filament\Resources\OrderResource\RelationManagers;
 use App\Models\Order;
-use Filament\Forms;
-use Filament\Forms\Form;
+use App\Models\Product;
 use Filament\Resources\Resource;
-use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\ToggleButtons;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
+use App\Filament\Resources\OrderResource\Pages;
+use Filament\Support\Enums\Currency;
+use Filament\Support\Facades\Filament;
+use Filament\Tables\Columns\SelectColumn;
+use Filament\Tables\Columns\TextColumn;
+
+use function Laravel\Prompts\text;
 
 class OrderResource extends Resource
 {
     protected static ?string $model = Order::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Group::make()->schema([
+                    Section::make('Order Information')->schema([
+                        Select::make('user_id')
+                        ->label('Customer')
+                        ->relationship('user', 'name')
+                        ->searchable()
+                        ->preload()
+                        ->required(),
+
+                        Select::make('payment_method')
+                        ->options([
+                            'stripe' => 'Stripe',
+                            'cod' => 'Cash on Delivery',
+                        ])
+                        ->required(),
+
+                        Select::make('payment_status')
+                        ->options([
+                            'pending' => 'Pending',
+                            'paid' => 'Paid',
+                            'failed' => 'Failed'
+                        ])
+                        ->default('pending')
+                        ->required(),
+
+                            ToggleButtons::make('status')
+                            ->inline()
+                            ->default('new')
+                            ->required()
+                            ->options([
+                                'new' => 'New',
+                                'processing' => 'Processing',
+                                'shipped' => 'Shipped',
+                                'delivered' => 'Delivered',
+                                'cancelled' => 'Cancelled'
+                            ])
+                            ->colors([
+                                'new' => 'info',
+                                'processing' => 'warning',
+                                'shipped' => 'success',
+                                'delivered' => 'success',
+                                'cancelled' => 'danger'
+                            ])
+                            ->icons([
+                                'new' => 'heroicon-m-sparkles',
+                                'processing' => 'heroicon-m-arrow-path',
+                                'shipped' => 'heroicon-m-truck',
+                                'delivered' => 'heroicon-m-check-badge',
+                                'cancelled' => 'heroicon-m-x-circle'
+                            ]),
+
+                            Select::make('currency')
+                            ->options([
+                                'php' => '₱',
+                                'usd' => '$',
+                                'eur' => '€',
+                                'gbp' => '£'
+                            ])
+                            ->default('php')
+                            ->required(),
+
+                            Select::make('shipping_method')
+                            ->options([
+                                'fedex' => 'FedEx',
+                                'ups' => 'UPS',
+                                'dhl' => 'DHL',
+                                'usps' => 'USPS'
+                            ]),
+
+                            Textarea::make('notes')
+                            ->columnSpanFull()
+                    ])->columns(2),
+
+                    Section::make('Order Items')->schema([
+                        Repeater::make('items')
+                        ->relationship()
+                        ->schema([
+                            Select::make('product_id')
+                            ->relationship('product', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required()
+                            ->distinct()
+                            ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                            ->columnSpan(4)
+                            ->reactive()
+                            ->afterStateUpdated(fn ($state, Set $set) => $set('unit_amount', Product::find($state)?-> price ?? 0))
+                            ->afterStateUpdated(fn ($state, Set $set) => $set('total_amount', Product::find($state)?-> price ?? 0)),
+
+
+                            TextInput::make('quantity')
+                            ->numeric()
+                            ->required()
+                            ->default(1)
+                            ->minValue(1)
+                            ->columnSpan(2)
+                            ->reactive()
+                            ->afterStateUpdated(fn($state, Set $set, Get $get) => $set('total_amount', $state*$get('unit_amount'))),
+                            
+                            TextInput::make('unit_amount')
+                            ->numeric()
+                            ->required()
+                            ->disabled()
+                            ->dehydrated()
+                            ->columnSpan(3),
+
+                            TextInput::make('total_amount')
+                            ->numeric()
+                            ->required()
+                            ->dehydrated()
+                            ->columnSpan(3),
+                        ])->columns(12),
+
+                        Placeholder::make('grand_total_placeholder')
+                            ->label('Grand Total')
+                            ->content(function (Get $get, Set $set) {
+                                 $total = 0;
+                                if (!$items = $get('items')) {
+                                     return '₱0.00';
+                                }
+
+                                foreach ($items as $item) {
+                                $total += $item['total_amount'] ?? 0;
+                            }
+                                $set('grand_total', $total);
+                                return '₱' . number_format($total, 2);
+                            }),
+
+                        Hidden::make('grand_total')
+                        ->default(0)
+                    ])
+                ])->columnSpanFull()
             ]);
     }
 
@@ -31,14 +180,64 @@ class OrderResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('user.name')
+                ->label('Customer')
+                ->sortable()
+                ->searchable(),
+
+                TextColumn::make('grand_total')
+                ->numeric()
+                ->sortable()
+                ->money('₱'),
+
+                TextColumn::make('payment_method')
+                ->sortable()
+                ->searchable(),
+
+                TextColumn::make('payment_status')
+                ->sortable()
+                ->searchable(),
+
+                TextColumn::make('currency')
+                ->sortable()
+                ->searchable(),
+
+                TextColumn::make('shipping_method')
+                ->sortable()
+                ->searchable(),
+
+
+                SelectColumn::make('status')
+                ->options([
+                    'new' => 'New',
+                    'processing' => 'Processing',
+                    'shipped' => 'Shipped',
+                    'delivered' => 'Delivered',
+                    'cancelled' => 'Cancelled'
+                ])
+                ->searchable()
+                ->sortable(),
+
+                TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+
+                TextColumn::make('updated_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
+                ActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -53,6 +252,16 @@ class OrderResource extends Resource
             //
         ];
     }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+     public static function getNavigationBadgeColor(): string|array|null
+     {
+        return static::getModel()::count() > 10 ? 'danger' : 'success';
+     }
 
     public static function getPages(): array
     {
