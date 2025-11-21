b5b934a1beba316d0140695cd47dc0d5f97817c6
unknown <rosit.lawrence@gmail.com>
2025-11-21 11:41:22 +0800

    E-commerce 12

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,8 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
     </head>
-    <body>
+    <body class="bg-slate-200 dark:bg-slate-700">
+        @livewire('partials.navbar')
+        <main>
         {{ $slot }}
+        </main>
+        @livewire('partials.footer')
+        @livewireScripts
     </body>
 </html>
