d7e5faf84dd8412c468e8a03787c9f3e72f907cf
unknown <rosit.lawrence@gmail.com>
2025-11-21 12:36:08 +0800

    E-commerce 13

--- app/Livewire/Homepage.php
+++ app/Livewire/Homepage.php
@@ -3,11 +3,20 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Models\Brand;
+use App\Models\Category;
 
+#[Title('Home Page - Online&Go')]
 class Homepage extends Component
 {
     public function render()
     {
-        return view('livewire.homepage');
+        $brands = Brand::where('is_active', 1)->get();
+        $categories = Category::where('is_active', 1)->get();
+        return view('livewire.homepage', [
+            'brands' => $brands,
+            'categories' => $categories,
+        ]);
     }
 }
