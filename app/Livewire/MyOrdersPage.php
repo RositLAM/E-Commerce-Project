dab41d764202f550dcda8a61958f3ba0e1b359d8
unknown <rosit.lawrence@gmail.com>
2025-11-25 08:45:40 +0800

    E-commerce 24

--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -3,11 +3,26 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Models\Order;
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\Auth;
 
+#[Title('My Orders')]
 class MyOrdersPage extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.my-orders-page');
+        $userId = Auth::id(); // safest
+
+        $my_orders = Order::where('user_id', $userId)
+            ->latest()
+            ->paginate(5);
+
+        return view('livewire.my-orders-page', [
+            'orders' => $my_orders,
+        ]);
     }
 }
